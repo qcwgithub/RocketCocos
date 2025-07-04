d607ad59ac6b1c4c41446518bd047f809d8007e5
qcwgithub <doitmyway@qq.com>
2025-07-04 10:04:56 +0800

    cell 90

--- assets/scripts/MySettings.ts
+++ assets/scripts/MySettings.ts
@@ -4,7 +4,7 @@ export class MySettings {
     public static readonly rotateDuration: number = 0.2;
     public static readonly previewDuration: number = 0.4;
     public static readonly fireDuration: number = 0.2;
-    public static readonly cellSize: number = 100;
+    public static readonly cellSize: number = 90;
     public static readonly moveSpeed: number = 400;
     public static readonly bigVec3: Vec3 = new Vec3(1.5, 1.5, 1);
     public static readonly extendTime: number = 60;
